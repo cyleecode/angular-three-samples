793c47cdb328a3fc49cbfd04beed0be908bd26e2
cyleecode <chunyitlee@gmail.com>
2023-06-21 10:06:05 +0800

    Create a cube object

--- src/app/components/hello-three/hello-three.component.ts
+++ src/app/components/hello-three/hello-three.component.ts
@@ -18,5 +18,49 @@ export class HelloThreeComponent implements AfterViewInit {
 
     constructor() {}
 
-    ngAfterViewInit(): void {}
+    ngAfterViewInit(): void {
+        this.square_scene.nativeElement.appendChild(renderer.domElement)
+    }
 }
+
+/* ! Scene */
+const scene = new THREE.Scene()
+/* ! Camera */
+let camera: any
+// Perspective projection (P)
+// view from camera
+// ->
+// Far clip plane -> normal ratio
+// Near clip plane -> smaller ratio
+
+// Orthographic projection (O)
+// view from camera
+// ->
+// Far clip plane &
+// Near clip plane -> same ratio
+camera = new THREE.PerspectiveCamera(
+    75, //75 degree
+    window.innerWidth / window.innerHeight, //display ratio
+    0.1, //near plane
+    1000 //far plane
+)
+
+/* ! Renderer */
+const renderer = new THREE.WebGLRenderer()
+//set size to whole window
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+//3D Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+camera.position.z = 3
+
+function animate() {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+
+    renderer.render(scene, camera)
+}
+animate()
